eb09a11434084deafb720f3df9d2fd9f39c4cd6a
Artem Osypenko <artoss1234@gmail.com>
2023-02-07 14:38:00 +0200

    commit 2

---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import './styles/main.css';
+
+
+
+import NavBar from './components/navBar/NavBar';
+import Home from './pages/Home';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      
+      <NavBar/>
+
+      <Home></Home>
+
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
